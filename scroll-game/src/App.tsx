032a910798538591b68def0e610f2b90438bd048
zeevg <zeevg@wix.com>
2021-08-16 23:58:38 +0300

    game

--- scroll-game/src/App.tsx
+++ scroll-game/src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {main} from './main';
 
 function App() {
+
+  main();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <canvas id="myCanvas" width="300" height="150" style={{border: '1px solid #d3d3d3'}}>
+          Your browser does not support the HTML5 canvas tag.
+        </canvas>
+      </div>
   );
 }
 
